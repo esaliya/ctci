73a8b1a12ec47b7f3dd7ad1b28f0bea2fa38f175
Saliya Ekanayake <sekanaya@cs.indiana.edu>
2014-09-11 02:06:23 -0400

    Initial

---
+++ src/main/java/c17/Q4.java
@@ -0,0 +1,16 @@
+package c17;
+
+public class Q4 {
+    /**
+     * Write an algorithm which finds the maximum of two numbers
+     * without using if-else or any other comparison operator
+     *
+     * @param x - one number
+     * @param y - the other number
+     * @return - the maximum of x and y
+     */
+    public int max(int x, int y){
+
+        return 0;
+    }
+}
