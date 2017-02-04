acc8533603d58d01b469ee596f08a4cd78ccaf83
jaliyae <jaliyae@gmail.com>
2017-02-04 13:25:08 -0800

    Q1.4

--- cpp/string/main.cpp
+++ cpp/string/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "strings.cpp"
 
 using namespace std;
 
+void Q1_4()
+{
+    vector<string> v;
+    v.push_back("aaa");
+    v.push_back("aabb");
+    v.push_back("aacbb");
+    v.push_back("aabc");
+    v.push_back("");
+
+    StringFunctions sf;
+    for (string s : v){
+        cout<<"string "<<s<<" is "<<(sf.Q1_4_PalindromePermutation(s) ? "a ":"not a ")<<"permutation of a palindrome."<<endl;
+    }
+};
+
 int main(int argc, char* argv[])
 {
-	cout<<"hello"<<endl;
-	return 0;
+    Q1_4();
+    cout<<"hello"<<endl;
+    int x =0 ;
+    cin >> x;
+    return 0;
 }
--- cpp/string/strings.cpp
+++ cpp/string/strings.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
-
-int main(int argc, char* argv[])
+class StringFunctions
 {
-	cout<<"hello"<<endl;
-	return 0;
-}
+public:
+    bool Q1_4_PalindromePermutation(string str)
+    {
+        if (str.length() == 0)
+        {
+            return true;
+        }
+
+        map<char,int> mmap;
+        for(char c: str)
+        {
+            if (mmap.find(c) != mmap.end())
+            {
+                mmap[c]++;
+            }
+            else
+            {
+                mmap.insert(pair<char, int>(c,1));
+            }
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for(map<char, int>::iterator it = mmap.begin(); it != mmap.end(); ++it)
+        {
+            if (it->second % 2 == 0)
+            {
+                evenCount++;
+            }
+            else
+            {
+                oddCount++;
+            }
+        }
+
+        if (oddCount == 0 || oddCount == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        } 
+    }
+};
