7c2fbad705ae36ffd245c1d85df70105ecfc2fac
jaliyae <jaliyae@gmail.com>
2017-02-05 19:33:26 -0800

    string complete

--- cpp/string/main.cpp
+++ cpp/string/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include "strings.cpp"
 
 using namespace std;
@@ -20,9 +21,122 @@ void Q1_4()
     }
 };
 
+void Q1_5()
+{
+    map<string, string> m;
+    m.insert(std::pair<string, string>("pale", "pal"));
+    m.insert(std::pair<string, string>("pales", "pale"));
+    m.insert(std::pair<string, string>("sale", "dale"));
+    m.insert(std::pair<string, string>("tale", "bake"));
+
+    StringFunctions sf;
+    for (map<string, string>::iterator it = m.begin(); it != m.end(); it++){
+        cout<<"string "<<it->first<<" and "<<it->second<<" are "<<(sf.Q1_5_OneEditAway(it->first, it->second) ? "":"not ")<<"one edit away."<<endl;
+    }
+}
+
+void Q1_6()
+{
+    vector<string> v;
+    v.push_back("aaaa");
+    v.push_back("aabbccdd");
+    v.push_back("aacbbeeee");
+    v.push_back("abcd");
+    v.push_back("");
+
+    StringFunctions sf;
+    for (string s : v){
+        cout<<"compressed string of "<<s<<" is "<<sf.Q1_6_StringCompression(s)<<endl;
+    }
+} 
+
+void Q1_7()
+{
+    const int N = 5;
+    int image[N][N];
+    for (int i = 0;  i < N; ++i)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            image[i][j] = i;
+            cout<<image[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<endl;
+
+    StringFunctions sf;
+    sf.Q1_7_TransposeAnImage<N,N>(image);
+    for (int i = 0;  i < N; ++i)
+    {
+        for (int j = 0; j < N; j++)
+        {                
+            cout<<image[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void Q1_8()
+{
+    const int N = 5;
+    int mat[N][N];
+    for (int i = 0;  i < N; ++i)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            mat[i][j] = i+1;          
+        }
+    }
+
+    mat[2][3] = 0;
+    mat[4][1] = 0;
+
+    for (int i = 0;  i < N; ++i)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout<<mat[i][j]<<" ";     
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<endl;
+
+    StringFunctions sf;
+    sf.Q1_8_ZeroMatrix<N,N>(mat);
+    for (int i = 0;  i < N; ++i)
+    {
+        for (int j = 0; j < N; j++)
+        {                
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void Q1_9()
+{
+    map<string, string> m;
+    m.insert(std::pair<string, string>("waterbottle", "erbottlewa"));
+     m.insert(std::pair<string, string>("caterbottle", "erbottlecat"));
+    m.insert(std::pair<string, string>("mail", "ilma"));
+  
+    StringFunctions sf;
+    for (map<string, string>::iterator it = m.begin(); it != m.end(); it++){
+        cout<<"string "<<it->first<<" and "<<it->second<<" are "<<(sf.Q1_9_IsRotation(it->first, it->second) ? "":"not ")<<"a rotaion."<<endl;
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    Q1_4();
+    //Q1_4();
+    //Q1_5();
+    //Q1_6();
+    //Q1_7();
+    //Q1_8();
+    Q1_9();
     cout<<"hello"<<endl;
     int x =0 ;
     cin >> x;
--- cpp/string/strings.cpp
+++ cpp/string/strings.cpp
@@ -2,11 +2,24 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 class StringFunctions
 {
 public:
+
+    bool Q1_2_IsPermutation(string s1, string s2)
+    {
+        std::sort(s1.begin(), s1.end());
+        std::sort(s2.begin(), s2.end());
+
+        if (s1.compare(s2) == 0 ) 
+            return true;
+        else
+            return false;
+    };
     bool Q1_4_PalindromePermutation(string str)
     {
         if (str.length() == 0)
@@ -50,5 +63,146 @@ public:
         {
             return false;
         } 
+    }
+
+    bool Q1_5_OneEditAway(string a, string b)
+    {
+        int aLen = a.length();
+        int bLen = b.length();
+        int errCnt = 0;
+        string other;
+
+        if (std::abs(aLen - bLen) > 1)
+            return false;
+
+        if (aLen < bLen) { other = b; }
+        else { other = a; a = b; }
+        aLen = a.length(); 
+
+        int aPos = 0;
+        int oPos = 0;
+
+        while (aPos < aLen)
+        {
+            if (a[aPos] != other[oPos])
+            {
+                errCnt++;
+            }
+
+            if (aLen == bLen)
+            {
+                aPos++;
+                oPos++;
+            }
+            else
+            {
+                oPos++;
+            }
+            if (errCnt > 1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string Q1_6_StringCompression(string str)
+    {
+        if (str.length() == 0) return str;
+
+        stringstream ss;
+        char prev = str[0];
+        int count = 0;
+        for (char s: str)
+        {
+            if (s == prev)
+            {
+                count++;
+            }
+            else
+            {
+                ss << prev;
+                ss << count;
+                prev = s;
+                count = 1;
+            }
+        }
+        if (count > 1)
+        {
+            ss << prev;
+            ss << count;
+        }
+
+        string tmp = ss.str();
+        if (tmp.length() > str.length())
+            return str;
+        else
+            return tmp;
+    }
+
+    template<size_t rows, size_t cols>
+    void Q1_7_TransposeAnImage(int (&image)[rows][cols])
+    {
+        if (rows != cols)
+        {
+            throw exception("rows and columns must be equal.");
+        }
+        size_t N = rows;
+
+        int tmp1, tmp2;
+
+        for (size_t i = 0;  i < ceil(N /2)  ; ++i)
+        {
+            for (size_t j = i; j < N -1- i; j++)
+            {                   
+                tmp1 = image[j][N-1-i];
+                image[j][N-1-i] = image[i][j];
+                tmp2 = image[N-1-i][N-1 -j];
+                image[N-1-i][N-1 -j]= tmp1;
+                tmp1 = image[N- 1-j][N-1-(N-1-i)];
+                image[N- 1-j][N-1-(N-1-i)]= tmp2;
+                image[i][j] =tmp1;                
+            }               
+        }        
+    }
+
+    template<size_t R, size_t C>
+    void Q1_8_ZeroMatrix(int (&mat)[R][C])
+    {
+        typedef std::pair<size_t, size_t> P;
+
+        vector<P> v;
+        for (size_t i = 0; i < R; ++i)
+        {
+            for (size_t j = 0; j < C; ++j)
+            {
+                if (mat[i][j] == 0)
+                {
+                    v.push_back(P(i,j));
+                }
+            }
+        }
+
+        for (P p: v)
+        {
+            for (size_t j = 0; j < C; ++j)
+            {
+                mat[p.first][j] = 0;
+            }
+            for (size_t i = 0; i < R; ++i)
+            {
+                mat[i][p.second] = 0;
+            }
+        }
+    }
+
+    bool Q1_9_IsRotation(string a, string b)
+    {
+        if (a.length() == b.length() && a.length() != 0)
+        {
+            string tmp = a + a;
+            return (tmp.find(b) != string::npos) ? true : false;
+        }
+        return false;
     }
 };
