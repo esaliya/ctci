c20622403910e6b7a56699d545a1386172ecdf7f
jaliyae <jaliyae@gmail.com>
2017-02-14 22:53:11 -0800

    Adding linkedlist

---
+++ cpp/linkedlist/linkedlist.cpp
@@ -0,0 +1,15 @@
+// linkedlist.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello from linkedlist" << endl;
+	int x;
+	cin >> x;
+
+    return 0;
+}
+
